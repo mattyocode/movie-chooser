fb1db18751b5e8fe7d2af9ea1d6400908870592a
mattyocode <matthewoliver@live.co.uk>
2021-01-02 21:04:51 +0000

    Index template created

--- config/settings.py
+++ config/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'moviechooser/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
