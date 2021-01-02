f57b1b8ef68492a0e68c9a6c5097daf8c9a3099d
mattyocode <matthewoliver@live.co.uk>
2021-01-02 20:10:00 +0000

    Test: index returns correct html passing

--- moviechooser/library/tests/test_views.py
+++ moviechooser/library/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.urls import resolve
+from django.http import HttpRequest
 from django.test import TestCase
 
 from moviechooser.library.views import index
@@ -8,3 +9,9 @@ class LibraryIndexTest(TestCase):
     def test_root_url_resolves_to_index_view(self):
         found = resolve('/')
         self.assertEqual(found.func, index)
+
+    def test_index_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        html = response.content.decode()
+        self.assertIn('<title>Movie Chooser</title>', html)
--- moviechooser/library/views.py
+++ moviechooser/library/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index():
-    pass
+def index(request):
+    return HttpResponse('<html><title>Movie Chooser</title></html>')
