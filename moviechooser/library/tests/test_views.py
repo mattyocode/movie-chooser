eeb7084b93abdede9b5fbf09819f3678ff1c11c0
mattyocode <matthewoliver@live.co.uk>
2021-01-07 11:11:29 +0000

    Added surprise.html template

--- moviechooser/library/tests/test_views.py
+++ moviechooser/library/tests/test_views.py
@@ -9,3 +9,7 @@ class LibraryIndexTest(TestCase):
     def test_uses_index_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_uses_surprise_template(self):
+        response = self.client.get('/surprise')
+        self.assertTemplateUsed(response, 'surprise.html')
--- moviechooser/library/views.py
+++ moviechooser/library/views.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from django.shortcuts import render
 
 from .models import Movie
@@ -6,3 +8,9 @@ from .models import Movie
 def index(request):
     movies = Movie.objects.all().order_by("-released")
     return render(request, 'index.html', {'movies': movies})
+
+def surprise(request):
+    movies = Movie.objects.all()
+    rand_index = randint(0, len(movies) - 1)
+    random_movie = movies[rand_index]
+    return render(request, 'surprise.html', {'movie': random_movie})
