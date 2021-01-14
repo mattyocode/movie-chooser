98dbfe59e4b29968921ea8f7ecb3fc7ea3ac4c3e
mattyocode <matthewoliver@live.co.uk>
2021-01-14 17:39:02 +0000

    test: mMovie model custom methods passing

--- moviechooser/library/tests/test_models.py
+++ moviechooser/library/tests/test_models.py
@@ -21,6 +21,70 @@ class MovieModelTest(TestCase):
         # actor_instance = Actor.objects.create(name="Wunman Show")
 
     def test_title_label(self):
+        """It uses correct label for title."""
         movie = Movie.objects.get(imdbid='test1234')
         field_label = movie._meta.get_field('title').verbose_name
         self.assertEqual(field_label, 'title')
+
+    def test_title_max_length(self):
+        """It has correct max_length for title."""
+        movie = Movie.objects.get(imdbid='test1234')
+        max_length = movie._meta.get_field('title').max_length
+        self.assertEqual(max_length, 200)
+
+    def test_str_returns_title(self):
+        """It returns title as string."""
+        movie = Movie.objects.get(imdbid='test1234')
+        result = movie.__str__()
+        self.assertEqual(result, 'Tester: Revenge of the Test')
+
+    def test_get_year_from_movie(self):
+        """It returns year only from released."""
+        movie = Movie.objects.get(imdbid='test1234')
+        result = movie.get_year()
+        self.assertEqual(result, '2021')
+
+    def test_get_genre(self):
+        """It returns single genre as string."""
+        movie = Movie.objects.get(imdbid='test1234')
+        movie.genre.add(Genre.objects.create(name="Horror"))
+        result = movie.get_genre()
+        self.assertEqual(result, 'Horror')
+
+    def test_get_multiple_genres(self):
+        """It returns 2 genres as string."""
+        movie = Movie.objects.get(imdbid='test1234')
+        movie.genre.add(Genre.objects.create(name="Horror"))
+        movie.genre.add(Genre.objects.create(name="Thriller"))
+        result = movie.get_genre()
+        self.assertEqual(result, 'Horror, Thriller')
+
+    def test_get_actor(self):
+        """It returns single actor as string."""
+        movie = Movie.objects.get(imdbid='test1234')
+        movie.actors.add(Actor.objects.create(name="Terry Ester"))
+        result = movie.get_actors()
+        self.assertEqual(result, 'Terry Ester')
+
+    def test_get_multiple_actors(self):
+        """It returns 2 actors as string."""
+        movie = Movie.objects.get(imdbid='test1234')
+        movie.actors.add(Actor.objects.create(name="Terry Ester"))
+        movie.actors.add(Actor.objects.create(name="Code Checker"))
+        result = movie.get_actors()
+        self.assertEqual(result, 'Terry Ester, Code Checker')
+
+    def test_get_director(self):
+        """It returns single director as string."""
+        movie = Movie.objects.get(imdbid='test1234')
+        movie.director.add(Director.objects.create(name="Christopher Codeman"))
+        result = movie.get_director()
+        self.assertEqual(result, 'Christopher Codeman')
+
+    def test_get_multiple_director(self):
+        """It returns 2 director as string."""
+        movie = Movie.objects.get(imdbid='test1234')
+        movie.director.add(Director.objects.create(name="Christopher Codeman"))
+        movie.director.add(Director.objects.create(name="David Lint"))
+        result = movie.get_director()
+        self.assertEqual(result, 'Christopher Codeman, David Lint')
