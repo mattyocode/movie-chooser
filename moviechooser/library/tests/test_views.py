a5a36a7a50a8454e3aea7d6821dfd49c6e502812
mattyocode <matthewoliver@live.co.uk>
2021-01-14 20:51:09 +0000

    test:  Movie detail view correct template and movie info

--- moviechooser/library/tests/test_views.py
+++ moviechooser/library/tests/test_views.py
@@ -15,6 +15,7 @@ class LibraryIndexTest(TestCase):
         response = self.client.get('/library/')     
         self.assertContains(response, 'All Movies')
 
+
 class LibrarySurpriseTest(TestCase):
     
     def test_uses_library_surprise_template(self):
@@ -29,7 +30,7 @@ class LibrarySurpriseTest(TestCase):
         response = self.client.get('/library/surprise/')
         self.assertTemplateUsed(response, 'surprise.html')
 
-    def test_displays_selected_movie(self):
+    def test_displays_when_1_movie(self):
         movie = Movie.objects.create(
             imdbid='test1234',
             title='Tester: Revenge of the Test',
@@ -42,6 +43,53 @@ class LibrarySurpriseTest(TestCase):
         self.assertContains(response, 'Tester: Revenge of the Test')
         self.assertContains(response, 'Surprise, Mother Flicker!')
 
+    def test_displays_when_2_movies(self):
+        movie_1 = Movie.objects.create(
+            imdbid='test1234',
+            title='Tester: Revenge of the Test',
+            released='2021-01-14',
+            runtime='100',
+            writer='Check Itt',
+            poster_url='www.example.com/image/location/img.jpg'
+        )
+        movie_2 = Movie.objects.create(
+            imdbid='test7890',
+            title='Tester 2: Test Harder',
+            released='2021-01-15',
+            runtime='111',
+            writer='Check Itt',
+            poster_url='www.example.com/image/location/img2.jpg'
+        )
+        response = self.client.get('/library/surprise/')
+        self.assertContains(response, 'Surprise, Mother Flicker!')
+        self.assertContains(response, 'Released: 2021')
 
-    # def test_uses_movie_detail_template(self):
-    #     movie = Movie.objects.
+
+class MovieDetailTest(TestCase):
+
+    def test_uses_movie_detail_template(self):
+        movie = Movie.objects.create(
+            imdbid='test1234',
+            title='Tester: Revenge of the Test',
+            released='2021-01-14',
+            runtime='100',
+            writer='Check Itt',
+            poster_url='www.example.com/image/location/img.jpg'
+        )
+        response = self.client.get('/library/movie/test1234/')
+        self.assertTemplateUsed(response, 'movie_detail.html')
+
+
+    def test_displays_selected_movie_detail(self):
+        movie = Movie.objects.create(
+            imdbid='test1234',
+            title='Tester: Revenge of the Test',
+            released='2021-01-14',
+            runtime='100',
+            writer='Check Itt',
+            poster_url='www.example.com/image/location/img.jpg'
+        )
+        response = self.client.get('/library/movie/test1234/')
+        self.assertContains(response, 'Tester: Revenge of the Test')
+        self.assertContains(response, 'Movie Details')
+        self.assertContains(response, 'Released: 2021')
