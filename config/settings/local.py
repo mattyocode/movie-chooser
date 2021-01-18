45a4b64bc0caba350f1dda123e03933614196d22
mattyocode <matthewoliver@live.co.uk>
2021-01-18 21:51:35 +0000

    first commit to heroku

--- config/settings/local.py
+++ config/settings/local.py
@@ -7,13 +7,24 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ["LOCAL_DB_NAME"],
+#         'USER': os.environ["LOCAL_DB_USER"],
+#         'PASSWORD': os.environ["LOCAL_DB_PASSWORD"],
+#         'HOST': os.environ["LOCAL_DB_HOST"],
+#         'PORT': os.environ["LOCAL_DB_PORT"],
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["LOCAL_DB_NAME"],
-        'USER': os.environ["LOCAL_DB_USER"],
-        'PASSWORD': os.environ["LOCAL_DB_PASSWORD"],
-        'HOST': os.environ["LOCAL_DB_HOST"],
-        'PORT': os.environ["LOCAL_DB_PORT"],
+        'NAME': os.environ["DB_NAME"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': os.environ["DB_HOST"],
+        'PORT': os.environ["DB_PORT"],
     }
 }
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
@@ -30,3 +30,9 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_REGION_NAME = 'eu-west-2'
+# AWS_LOCATION = 'static'
+
+# HEROKU CONFIG
+
+ALLOWED_HOSTS += ['movie-chooser-mo1.herokuapp.com/']
