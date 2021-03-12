c35875dc639a64c652a3247bf9e11906ff16ab8c
mattyocode <matthewoliver@live.co.uk>
2021-03-12 18:02:10 +0000

    feat: added fields to Item model

--- moviechooser/lists/models.py
+++ moviechooser/lists/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Item(models.Model):
 
-    # user = 
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
-    # date_added =
-    # watched = 
+    date_added = models.DateTimeField(auto_now_add=True)
+    watched = models.BooleanField(default=False)
