ecbfe0461ef271c0f3c45f61c25f518207e05811
mattyocode <matthewoliver@live.co.uk>
2021-03-13 12:50:00 +0000

    feat: order Items by date_added

--- moviechooser/lists/models.py
+++ moviechooser/lists/models.py
@@ -8,3 +8,9 @@ class Item(models.Model):
     title = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     watched = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['date_added']
