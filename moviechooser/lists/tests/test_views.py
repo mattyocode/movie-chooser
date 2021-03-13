4ffd940f990378fd5794ec4335bd8fd5c62ea3f4
mattyocode <matthewoliver@live.co.uk>
2021-03-13 16:46:32 +0000

    feat: my_list view adds Item to db

--- moviechooser/lists/tests/test_views.py
+++ moviechooser/lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from moviechooser.lists.views import my_list
+from moviechooser.lists.models import Item
 
 class ListViewTest(TestCase):
 
@@ -12,4 +13,11 @@ class ListViewTest(TestCase):
     def test_can_save_POST_request(self):
         data = {'imdbid': 'abc123'}
         response = self.client.post(reverse('lists:my_list'), data)
-        self.assertIn('abc123', response.content.decode())
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.imdbid, 'abc123')
+
+    def test_doesnt_save_when_not_POST_request(self):
+        response = self.client.get(reverse('lists:my_list'))
+        self.assertEqual(Item.objects.count(), 0)
--- moviechooser/lists/views.py
+++ moviechooser/lists/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+
+from moviechooser.lists.models import Item
 
 # Create your views here.
 
 def my_list(request):
-    return render(request, 'my_list.html', {
-        'new_movie_item': request.POST.get('imdbid', ''),
-    })
+    if request.method == 'POST':
+        item_imdbid = request.POST['imdbid']
+        Item.objects.create(imdbid=item_imdbid)
+        return HttpResponse('<script>history.back();</script>')
+
+    return render(request, 'my_list.html')
