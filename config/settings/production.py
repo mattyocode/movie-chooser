cc9aae283eed9465eb15d898b8de903a48c958e2
mattyocode <matthewoliver@live.co.uk>
2021-06-19 20:05:34 +0100

    add cache config to production

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,6 +18,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 INSTALLED_APPS += ['storages']
 
 # AWS S3 CONFIG
